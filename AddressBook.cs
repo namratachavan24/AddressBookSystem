69363a1416b6be2d682cea44d1be9e06a3120ff3
namratachavan24 <namrata2015.nc@gmail.com>
2022-08-13 23:59:51 +0530

    Uc1 to Uc4 Added

---
+++ AddressBook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystemDay9
+{
+    public class AddressBook
+    {
+      
+        public List<Contact> ContactList;
+        public AddressBook()
+        {
+            this.ContactList = new List<Contact>();
+        }
+        public void AddContact(Contact contactObj)
+        {
+            this.ContactList.Add(contactObj);
+        }
+        public int FindByPhoneNum(long phoneNumber)
+        {
+            return this.ContactList.FindIndex(contact => contact.PhoneNumber.Equals(phoneNumber));
+        }
+        public int FindByFirstName(string firstName)
+        {
+            return this.ContactList.FindIndex(contact => contact.FirstName.Equals(firstName));
+        }
+        //Delete a Give Contact By Index
+        public void DeleteContact(int index)
+        {
+            this.ContactList.RemoveAt(index);
+        }
+    }
+}
+
+
+   
